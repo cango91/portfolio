66cc7112f62be8fb46d468c82e9288b6e6e59498
cango91 <can.gologlu@gmail.com>
2023-08-29 21:34:26 -0400

    GA fix attempt 2. Clean unused code

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import ReactGa from 'react-ga';
-ReactGa.initialize('G-DFWZWR80MF');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
